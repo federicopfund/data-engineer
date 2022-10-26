378aca1d8f0488df3f29b301a2d8859e435ca8e8
jmartinelena <jmartinelena1@gmail.com>
2022-10-26 14:21:43 -0300

    Agrego transformaciones 6 y 7, y tambien agrego scrips de creacion de sus DDBB

---
+++ Scripts sql/Transformacion6.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[Transformacion6] (
+	Cod_Producto VARCHAR(20) NOT NULL,
+	Producto VARCHAR(50) NOT NULL,
+	Cod_SubCategoria VARCHAR(20) NOT NULL,
+	Color VARCHAR(20) NOT NULL);
---
+++ Scripts sql/Transformacion7.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[Transformacion7] (
+	Cod_SubCategoria VARCHAR(5) NOT NULL,
+	SubCategoria VARCHAR(20) NOT NULL,
+	Cod_Categoria VARCHAR(5) NOT NULL);
