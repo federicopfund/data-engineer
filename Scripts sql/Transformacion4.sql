9db8b983a806e2dcdd5b65a1af337df80e0f7c27
jmartinelena <jmartinelena1@gmail.com>
2022-10-26 13:53:44 -0300

    Agrego transformaciones 4 y 5, y tambien agrego scrips de creacion de sus DDBB

---
+++ Scripts sql/Transformacion4.sql
@@ -0,0 +1,4 @@
+CREATE TABLE dbo.Transformacion4 (
+	Country VARCHAR(25) NOT NULL, 
+	Suma_TotalWasted decimal(29,4) NOT NULL
+	)
---
+++ Scripts sql/Transformacion5.sql
@@ -0,0 +1,2 @@
+CREATE TABLE [dbo].[Transformacion5] (
+	Cantidad_CodProducto INT NOT NULL);
