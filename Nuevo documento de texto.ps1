0779846d8b131c5a8a609fd576cfa4c37ca2da6b
marconcz <marconcz@gmail.com>
2022-11-02 13:07:03 -0300

    Mensaje automatizado

--- Nuevo documento de texto.ps1
+++ Nuevo documento de texto.ps1
@@ -1,7 +1,10 @@
-cd notebooks
 $clave = 1234
-"storageaccount=$clave"|Set-Content keys.txt
-"storageaccountkey"|Add-Content keys.txt
+$clave2 = 12345
+"storageaccountname"|Set-Content keys.txt
+$clave|Add-Content keys.txt
+
+"storageaccoun2"|Add-Content keys.txt
+$clave2|Add-Content keys.txt
 
 git pull
 git add .
