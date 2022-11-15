71efdae34ad971fe4a06844a88ab61209903b177
jmartinelena <jmartinelena1@gmail.com>
2022-11-15 11:34:02 -0300

    Agrego comentarios y checkeo basico de errores

--- api.py
+++ api.py
@@ -15,50 +15,78 @@ app = FastAPI()
 @app.get("/productos")
 async def getProductos():
     '''Devuelve todos los productos'''
+    # Lee dataframes
     df = pd.read_csv("Datasets locales/Producto_Unico.csv")
     df2 = pd.read_csv('Datasets locales/Producto_Sucursales.csv')
 
+    # Crea una columna unica con sucursal y stock
     df2['Sucursal_y_Stock'] = df2.apply(lambda row: {'Cod_Sucursal' : row['Cod_Sucursal'], 'Stock' : row['Stock']}, axis=1)
+    # Colapsa las 10 filas por cada producto en una sola con un array de diccionarios
     suc_collapsed = df2[['Cod_Producto', 'Sucursal_y_Stock']].groupby('Cod_Producto').agg(lambda x: x.tolist())
+    # Joinea los productos con stock
     join = df.set_index('Cod_Producto').join(suc_collapsed, on='Cod_Producto', how='inner')
 
     return {"dataframe" : json.loads(join.to_json(orient='records'))}
+
 
 @app.get("/productos/{id_producto}")
 async def getProductoById(id_producto: int):
     '''Devuelve el producto que tenga cierto ID'''
+    # Lee dataframes
     df = pd.read_csv("Datasets locales/Producto_Unico.csv")
     df2 = pd.read_csv('Datasets locales/Producto_Sucursales.csv')
 
+    # Filtra por el producto pedido
     df = df.loc[df['Cod_Producto'] == id_producto]
+    # Crea una columna unica con sucursal y stock
     df2['Sucursal_y_Stock'] = df2.apply(lambda row: {'Cod_Sucursal' : row['Cod_Sucursal'], 'Stock' : row['Stock']}, axis=1)
+    # Colapsa las 10 filas por cada producto en una sola con un array de diccionarios
     suc_collapsed = df2[['Cod_Producto', 'Sucursal_y_Stock']].groupby('Cod_Producto').agg(lambda x: x.tolist())
+    # Joinea los productos con stock
     join = df.set_index('Cod_Producto').join(suc_collapsed, on='Cod_Producto', how='inner')
 
-    return {"dataframe" : json.loads(join.to_json(orient='records'))}
+    if df.shape[0] != 0:
+        return {"dataframe" : json.loads(join.to_json(orient='records'))}
+    else:
+        return {"error" : f"No existe el producto con id {id_producto}"}
+
 
 @app.get("/categorias/{id_categoria}")
 async def getProductoByCategory(id_categoria: int):
     '''Devuelve los productos bajo cierta categoria'''
+    # Lee dataframes
     df = pd.read_csv("Datasets locales/Producto_Unico.csv")
     df2 = pd.read_csv('Datasets locales/Producto_Sucursales.csv')
 
+    # Filtra por la categoria pedida
     df = df.loc[df['Cod_Categoria'] == id_categoria]
+    # Crea una columna unica con sucursal y stock
     df2['Sucursal_y_Stock'] = df2.apply(lambda row: {'Cod_Sucursal' : row['Cod_Sucursal'], 'Stock' : row['Stock']}, axis=1)
+    # Colapsa las 10 filas por cada producto en una sola con un array de diccionarios
     suc_collapsed = df2[['Cod_Producto', 'Sucursal_y_Stock']].groupby('Cod_Producto').agg(lambda x: x.tolist())
+    # Joinea los productos con stock
     join = df.set_index('Cod_Producto').join(suc_collapsed, on='Cod_Producto', how='inner')
 
-    return {"dataframe" : json.loads(join.to_json(orient='records'))}
+    if df.shape[0] != 0:
+        return {"dataframe" : json.loads(join.to_json(orient='records'))}
+    else:
+        return {"error" : f"No existe la categoria con id {id_categoria}"}
+
 
 @app.get("/categorias/{id_categoria}/subcategorias/{id_subcategoria}")
 async def getProductoByCategory(id_categoria: int, id_subcategoria: int):
     '''Devuelve los productos bajo cierta categoria que corresponden a una subcategoria'''
+    # Lee dataframes
     df = pd.read_csv("Datasets locales/Producto_Unico.csv")
     df2 = pd.read_csv('Datasets locales/Producto_Sucursales.csv')
 
+    # Filtra por la subcategoria y categoria pedida
     df = df.loc[(df['Cod_Categoria'] == id_categoria) & (df['Cod_Subcategoria'] == id_subcategoria)]
+    # Crea una columna unica con sucursal y stock
     df2['Sucursal_y_Stock'] = df2.apply(lambda row: {'Cod_Sucursal' : row['Cod_Sucursal'], 'Stock' : row['Stock']}, axis=1)
+    # Colapsa las 10 filas por cada producto en una sola con un array de diccionarios
     suc_collapsed = df2[['Cod_Producto', 'Sucursal_y_Stock']].groupby('Cod_Producto').agg(lambda x: x.tolist())
+    # Joinea los productos con stock
     join = df.set_index('Cod_Producto').join(suc_collapsed, on='Cod_Producto', how='inner')
 
     if df.shape[0] != 0:
@@ -66,15 +94,24 @@ async def getProductoByCategory(id_categoria: int, id_subcategoria: int):
     else:
         return {"error" : f"La subcategoria {id_subcategoria} no pertenece a la categoria {id_categoria}"}
 
+
 @app.get("/subcategorias/{id_subcategoria}")
 async def getProductoByCategory(id_subcategoria: int):
     '''Devuelve los productos bajo cierta subcategoria'''
+    # Lee dataframes
     df = pd.read_csv("Datasets locales/Producto_Unico.csv")
     df2 = pd.read_csv('Datasets locales/Producto_Sucursales.csv')
 
+    # Filtra por la subcategoria
     df = df.loc[df['Cod_Subcategoria'] == id_subcategoria]
+    # Crea una columna unica con sucursal y stock
     df2['Sucursal_y_Stock'] = df2.apply(lambda row: {'Cod_Sucursal' : row['Cod_Sucursal'], 'Stock' : row['Stock']}, axis=1)
+    # Colapsa las 10 filas por cada producto en una sola con un array de diccionarios
     suc_collapsed = df2[['Cod_Producto', 'Sucursal_y_Stock']].groupby('Cod_Producto').agg(lambda x: x.tolist())
+    # Joinea los productos con stock
     join = df.set_index('Cod_Producto').join(suc_collapsed, on='Cod_Producto', how='inner')
     
-    return {"dataframe" : json.loads(join.to_json(orient='records'))}
+    if df.shape[0] != 0:
+        return {"dataframe" : json.loads(join.to_json(orient='records'))}
+    else:
+        return {"error" : f"No existe subcategoria con id {id_subcategoria}"}
