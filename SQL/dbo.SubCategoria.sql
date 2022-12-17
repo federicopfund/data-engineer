bb940088a24bea30f31191e8d39c2ac79d1dcb7b
federico pfund <federicopfund@gmail.com>
2022-12-17 05:58:48 -0300

    Create table `SubCategoria`

---
+++ SQL/dbo.SubCategoria.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[SubCategoria] (
+    [Cod_SubCategoria] VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
+    [SubCategoria]     VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
+    [Cod_Categoria]    VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL
+);
+
