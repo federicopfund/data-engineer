65fc2715cb75d4b3f164e828b02d2ee475b10b00
federico pfund <federicopfund@gmail.com>
2022-12-17 05:57:54 -0300

    Create table `Mine`

---
+++ SQL/dbo.Mine.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Mine] (
+    [TruckID]       INT           NOT NULL,
+    [Truck]         VARCHAR (65)  NULL,
+    [ProjectID]     INT           NOT NULL,
+    [Country]       NVARCHAR (50) NOT NULL,
+    [OperatorID]    INT           NOT NULL,
+    [FirstName]     NVARCHAR (50) NULL,
+    [LastName]      NVARCHAR (50) NULL,
+    [Age]           INT           NULL,
+    [TotalOreMined] MONEY         NOT NULL,
+    [TotalWasted]   MONEY         NOT NULL,
+    [Date]          DATETIME      NULL
+);
+
