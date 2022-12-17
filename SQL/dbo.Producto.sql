6bd7dc4bd20a88c6b2aeff6d7a83122cc40b4527
federico pfund <federicopfund@gmail.com>
2022-12-17 05:58:19 -0300

    Create Table `Producto`

---
+++ SQL/dbo.Producto.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Producto] (
+    [Cod_Producto]     VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
+    [Producto]         VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
+    [Cod_SubCategoria] VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
+    [Color]            VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL
+);
+
