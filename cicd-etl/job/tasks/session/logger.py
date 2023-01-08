ecfbb7dd7c0e880a32efd08b4aba2c0d41139b52
federicopfund <federicopfund@gmail.com>
2023-01-08 07:18:45 -0300

    clase logger spark instancia.

--- cicd-etl/job/tasks/session/logger.py
+++ cicd-etl/job/tasks/session/logger.py
@@ -1,15 +1,12 @@
 
 # https://logging.apache.org/log4j/2.x/log4j-users-guide.pdf
 
-class Log4j(object) -> logger:
+class Log4j(object):
     """
     Clase Wrapper para el objeto Log4j JVM.
     :param spark: objeto SparkSession.
     """
-    def __init__(self, spark) -> LogManager:
-         """ Obtener detalles de la aplicación Spark
-              Contructor_
-         """
+    def __init__(self, spark):
         log4j = spark._jvm.org.apache.log4j
         conf = spark.sparkContext.getConf()
         app_id = conf.get('spark.app.id')
@@ -17,30 +14,23 @@ class Log4j(object) -> logger:
         message_pre  = '<' + app_name + ' ' + app_id + '>'
         self.logger = log4j.LogManager.getLogger(message_pre)
 
-    def warn(self, message) -> None:
+    def warn(self, message):
         """Registra una advertencia.
             :param: Mensaje de advertencia para escribir en el registro
             :Volver a la lista:
         """
         self.logger.warn(message)
-        return None
 
-    def info(self, message) -> None:
-         """Información de registro.
-                :param: Mensaje de información para escribir en el registro
-                :Volver a la lista:
-        """
-        self.logger.info(message)
-        return None
-
-    def error(self, message) -> None:
+    def info(self, message):
+        self.logger.info(message)   
+    
+    def error(self, message):
         """Registra un error.
             :param: mensaje de error para escribir en el registro
             :Volver a la lista:
         """
         self.logger.error(message)
-        return None
+    
 
-    def debug(self, message) -> None:
+    def debug(self, message):
         self.logger.debug(message)
-        return None
