ec47d2de62f5de3729130c3cd0eec8fc39f6af4c
federicopfund <federicopfund@gmail.com>
2023-12-03 13:49:18 -0300

    Tabla  sucursales

---
+++ SQL/Tablas/dbo.Sucursales.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Sucursales] (
+    [Cod_Sucursal]    VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
+    [Cod_Sucursal_PK] INT COLLATE Modern_Spanish_CI_AS NULL,
+    [Sucursal]        VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
+    [Latitud]         FLOAT COLLATE Modern_Spanish_CI_AS NULL,
+    [Longitud]        FLOAT COLLATE Modern_Spanish_CI_AS NULL
+);
+
