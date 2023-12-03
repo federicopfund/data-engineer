c0f74be0e93b798cb2cdd8f9bc848a5a4f1a638f
federicopfund <federicopfund@gmail.com>
2023-12-03 14:15:41 -0300

    Procedimiento para agregar un nuevo cliente

---
+++ SQL/Stored Procedure/Agrega_cliente.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE PROCEDURE add_customer(
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    address VARCHAR(255)
+)
+AS
+$$
+BEGIN
+    INSERT INTO customers (first_name, last_name, email, password, address)
+    VALUES (first_name, last_name, email, password, address);
+END;
+$$ LANGUAGE plpgsql;
