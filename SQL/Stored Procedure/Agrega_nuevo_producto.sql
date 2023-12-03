d7cc8a4338e3a6566b995534572b225a2648fc52
federicopfund <federicopfund@gmail.com>
2023-12-03 14:20:08 -0300

    Procedimiento para agregar un nuevo producto

---
+++ SQL/Stored Procedure/Agrega_nuevo_producto.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE PROCEDURE add_product(
+    product_code VARCHAR(20) UNIQUE,
+    product_name VARCHAR(255),
+    category_id INT,
+    price DECIMAL(10, 2),
+    stock_quantity INT,
+    description TEXT
+)
+AS
+$$
+BEGIN
+    INSERT INTO products (product_code, product_name, category_id, price, stock_quantity, description)
+    VALUES (product_code, product_name, category_id, price, stock_quantity, description);
+END;
+$$ LANGUAGE plpgsql;
