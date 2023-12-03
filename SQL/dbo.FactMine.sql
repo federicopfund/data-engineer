6557f7b7b16320b723e704dbb97e185bbeda9004
federicopfund <federicopfund@gmail.com>
2023-12-03 13:06:09 -0300

    refatored

--- SQL/dbo.FactMine.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[FactMine] (
-    [TruckID]       INT      NOT NULL,
-    [ProjectID]     INT      NOT NULL,
-    [OperatorID]    INT      NOT NULL,
-    [TotalOreMined] MONEY    NOT NULL,
-    [TotalWasted]   MONEY    NOT NULL,
-    [Date]          DATETIME NULL
-);
-
--- SQL/dbo.SubCategoria.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[SubCategoria] (
-    [Cod_SubCategoria] VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
-    [SubCategoria]     VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL,
-    [Cod_Categoria]    VARCHAR (50) COLLATE Modern_Spanish_CI_AS NULL
-);
-
