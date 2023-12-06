e43abceb280fd5acad582ce71c460b75cfce6493
federicopfund <federicopfund@gmail.com>
2023-12-06 20:11:44 -0300

    procedimiento almacenado sin la restricción UNIQUE

--- SQL/Stored Procedure/Agrega_cliente.sql
+++ SQL/Stored Procedure/Agrega_cliente.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE PROCEDURE add_customer(
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    email VARCHAR(255) UNIQUE,
+    email VARCHAR(255),
     password VARCHAR(255),
     address VARCHAR(255)
 )
