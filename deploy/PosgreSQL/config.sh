b24bc4d98a43380d95f91c3af5326e9df55240b6
federicopfund <federicopfund@gmail.com>
2023-12-06 19:02:06 -0300

    Esto ejecuta el comando psql dentro del contenedor llamado postgres, utilizando el usuario -U fedex y conectándose a la base de datos -d ecommerce. Asegúrate de cambiar los valores según tu configuración.

--- deploy/PosgreSQL/config.sh
+++ deploy/PosgreSQL/config.sh
@@ -5,3 +5,6 @@ sudo chown $USER:$USER docker-compose.yml
 sudo chmod 666 /var/run/docker.sock
 groups
 
+
+## Para acceder al servicio de PosgreSQL
+docker exec -it postgres psql -U fedex -d ecommerce
