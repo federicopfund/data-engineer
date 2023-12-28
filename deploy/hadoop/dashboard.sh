c7b87300ee74941e28eea7e742240910c25f1f1b
federicopfund <federicopfund@gmail.com>
2023-12-27 21:05:55 -0300

    Comandos para el portainer de docker

---
+++ deploy/hadoop/dashboard.sh
@@ -0,0 +1,3 @@
+sudo docker volume create portainer_data
+sudo docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+xdg-open http://localhost:9000
