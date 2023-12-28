ed131c907aa0ffaf387ca5fbf99945426b4a5df6
federicopfund <federicopfund@gmail.com>
2023-12-28 18:17:29 -0300

    Comandos para crear directorio y cambiar autorizacion.

--- deploy/hadoop/comandos.sh
+++ deploy/hadoop/comandos.sh
@@ -1,3 +1,6 @@
+docker exec -it namenode hadoop fs -mkdir /user
+docker exec -it namenode hadoop fs -mkdir /user/fede
 docker exec -it namenode hadoop fs -mkdir /user/fede/landing
-hadoop fs -chmod 777 /user/fede/landing/csv
-hadoop fs -chown fede:hadoop /user/fede/landing/csv
+docker exec -it namenode hadoop fs -mkdir /user/fede/landing/csv
+docker exec -it namenode hadoop fs -chmod 777 /user/fede/landing/csv
+docker exec -it namenode hadoop fs -chown fede:hadoop /user/fede/landing/csv
