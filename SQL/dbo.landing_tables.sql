1622b28f801c9b7653b8c0361fc5e9c5f177f1bd
federicopfund <federicopfund@gmail.com>
2023-12-27 21:13:45 -0300

    delete de los servicios de sql

--- SQL/dbo.landing_tables.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[landing_tables] (
-    [TABLE]       VARCHAR (50)  NULL,
-    [SQL_SCRIPT]  VARCHAR (200) NOT NULL,
-    [ENABLE]      VARCHAR (1)   NULL,
-    [DESTINATION] VARCHAR (100) NULL
-);
-
