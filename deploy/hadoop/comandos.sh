e34852fa10a9bcd310b2b29ce1316f46829f1657
federicopfund <federicopfund@gmail.com>
2023-12-30 07:59:15 -0300

    Nuevos comandos hdfs

--- deploy/hadoop/comandos.sh
+++ deploy/hadoop/comandos.sh
@@ -4,3 +4,5 @@ docker exec -it namenode hadoop fs -mkdir /user/fede/landing
 docker exec -it namenode hadoop fs -mkdir /user/fede/landing/csv
 docker exec -it namenode hadoop fs -chmod 777 /user/fede/landing/csv
 docker exec -it namenode hadoop fs -chown fede:hadoop /user/fede/landing/csv
+docker exec -it namenode hdfs dfsadmin -report
+docker exec -it namenode hdfs dfsadmin -refreshNodes
