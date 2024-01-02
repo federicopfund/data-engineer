4db5f7cc69698405a41e0bc3bcfb0ee342f26fa4
federicopfund <federicopfund@gmail.com>
2024-01-02 18:43:05 -0300

    Comandos para apache hadoop.

--- deploy/hadoop/comandos.sh
+++ deploy/hadoop/comandos.sh
@@ -3,6 +3,8 @@ docker exec -it namenode hadoop fs -mkdir /user/fede
 docker exec -it namenode hadoop fs -mkdir /user/fede/landing
 docker exec -it namenode hadoop fs -mkdir /user/fede/landing/csv
 docker exec -it namenode hadoop fs -chmod 777 /user/fede/landing/csv
+docker exec -it namenode hadoop fs -chmod 777 /user/fede/landing/csv/transform
+docker exec -it namenode hadoop fs -chown fede:hadoop /user/fede/landing/csv/transform
 docker exec -it namenode hadoop fs -chown fede:hadoop /user/fede/landing/csv
 docker exec -it namenode hdfs dfsadmin -report
 docker exec -it namenode hdfs dfsadmin -refreshNodes
