853e8eb04b44d2b555bae34e45896fc6a6722fe9
federicopfund <federicopfund@gmail.com>
2024-01-02 18:41:01 -0300

    Main donde llamo las funciones.

--- cicd-etl/job/tasks/ScalaETL/src/main/scala/main/main.scala
+++ cicd-etl/job/tasks/ScalaETL/src/main/scala/main/main.scala
@@ -1,22 +1,17 @@
-import org.apache.spark.sql.SparkSession
+
 import scala.sparkSession.SparkSessionSingleton
 import scala.etl.Functions_etl
-import scala.hadoop.HadoopConnection
+import scala.hadoop.{Hadoop,DockerHadoop}
+import org.apache.spark.sql.SparkSession
+
 
 
 object MainETL {
 
-
   def main(args: Array[String]): Unit = {
-   
     val archivos = Array("Categoria.csv", "FactMine.csv", "Mine.csv", "Producto.csv", "VentasInternet.csv")
     val spark = SparkSessionSingleton.getSparkSession
-
-    try {
-      
-      Functions_etl.processPatternsParallel(spark, archivos)
-    } finally {
-      spark.stop()
-    }
+    Functions_etl.processPatternsParallel(spark, archivos)      
+    spark.stop() 
   }
 }
