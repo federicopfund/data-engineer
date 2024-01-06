69dd1acf06bf0120dd973ca85ba8631f70de046b
federicopfund <federicopfund@gmail.com>
2024-01-06 17:18:05 -0300

    main de etl

--- cicd-etl/job/tasks/ScalaETL/src/main/scala/main.scala
+++ cicd-etl/job/tasks/ScalaETL/src/main/scala/main.scala
@@ -1,18 +1,29 @@
-
 package main
 
 import scala.sparkSession.SparkSessionSingleton
-import scala.transform.funcTranform
+import scala.transform.{funcTranform, Hdfs}
 import org.apache.spark.sql.SparkSession
-
-
+import org.apache.log4j.PropertyConfigurator
 
 object MainETL {
 
   def main(args: Array[String]): Unit = {
+    // Configure Log4j
+    PropertyConfigurator.configure(getClass.getResource("/log4j.properties"))
+
+    // Array of files to be processed
     val archivos = Array("Categoria.csv", "FactMine.csv", "Mine.csv", "Producto.csv", "VentasInternet.csv")
+
+    // Run HDFS script to handle HDFS operations
+    Hdfs.runloadinHdfs(archivos)
+
+    // Get SparkSession using the SparkSessionSingleton
     val spark = SparkSessionSingleton.getSparkSession
-    funcTranform.processFile(spark, archivos)      
-    spark.stop() 
+
+    // Process files using Spark
+    funcTranform.processFile(spark, archivos)
+
+    // Stop SparkSession
+    spark.stop()
   }
 }
