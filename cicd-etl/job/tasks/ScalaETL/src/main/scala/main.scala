7ff0af16cf6be25b4aecefc2e78ed620561cd6f9
federicopfund <federicopfund@gmail.com>
2024-01-07 15:37:14 -0300

    modificaion de ip de namenode para la url de hdfs

--- cicd-etl/job/tasks/ScalaETL/src/main/scala/main.scala
+++ cicd-etl/job/tasks/ScalaETL/src/main/scala/main.scala
@@ -5,7 +5,7 @@ import scala.transform.{funcTransform,hdfs}
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.PropertyConfigurator
 import org.apache.hadoop.conf.Configuration
-
+import sys.process._
 
 object MainETL {
 
@@ -13,19 +13,17 @@ object MainETL {
     PropertyConfigurator.configure(getClass.getResource("/log4j.properties"))
     val hadoopConf = new Configuration()
     val archivos = Array("Categoria.csv", "FactMine.csv", "Mine.csv", "Producto.csv", "VentasInternet.csv")
-    val hdfsUri = "hdfs://172.19.0.5:9000"
+    val containerName = "namenode"
+    val command = Seq("sh", "-c", s"""docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $containerName""")
+    val result = command.!!
+    val numeros: Array[Int] = result.trim.split('.').map(_.toInt)
+    val direccionIP: String = numeros.mkString(".")
+    val hdfsUri: String = s"hdfs://$direccionIP:9000"
     val hdfsWorkingDir = "/user/fede/landing/csv"
-
-    // Call the method from the Hdfs object
     hdfs.runloadinHdfs(hadoopConf, archivos, hdfsUri, hdfsWorkingDir)
-
-    // Get SparkSession using the SparkSessionSingleton
     val spark = SparkSessionSingleton.getSparkSession
-
-    // Process files using Spark
     funcTransform.processFile(spark, archivos)
-
-    // Stop SparkSession
     spark.stop()
   }
 }
+
