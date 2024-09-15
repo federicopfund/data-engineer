775919fa5cdec62f55ae0e3b233b3edc14335dac
federicopfund <federicopfund@gmail.com>
2024-09-15 19:28:22 -0300

    feat: añadir funciones de transformación para archivos CSV en Spark- - Se implementaron funciones para transformar diferentes archivos CSV utilizando Spark:   - `transformCategoria`: Procesa el archivo Categoria.csv aplicando múltiples transformaciones SQL.   - `transformFactMine`: Procesa el archivo FactMine.csv con agregaciones específicas.   - `transformMine`: Procesa el archivo Mine.csv con varias transformaciones y agrupaciones.   - `transformProduct`: Procesa el archivo Producto.csv con cálculos sobre los productos.   - `transformVentasInternet`: Procesa el archivo VentasInternet.csv, aplicando transformaciones sobre las ventas. - Se añadió un sistema de caché para evitar cálculos innecesarios si los DataFrames ya están en caché. - Se añadieron funciones utilitarias para:   - Verificar si un archivo ya ha sido transformado (`fileExists`).   - Crear directorios para almacenar archivos transformados (`createDirectory`).   - Eliminar directorios y archivos previos antes de nuevas transformaciones (`deleteDirectory`).   - Guardar y mostrar los resultados de las transformaciones (`saveAndShow`). - Se gestionaron correctamente los recursos, asegurando que los DataFrames se liberen después de ser utilizados.

--- cicd-etl/job/tasks/ScalaETL/src/main/scala/main.scala
+++ cicd-etl/job/tasks/ScalaETL/src/main/scala/main.scala
@@ -11,16 +11,7 @@ object MainETL {
 
   def main(args: Array[String]): Unit = {
     PropertyConfigurator.configure(getClass.getResource("/log4j.properties"))
-    val hadoopConf = new Configuration()
     val archivos = Array("Categoria.csv", "FactMine.csv", "Mine.csv", "Producto.csv", "VentasInternet.csv")
-    val containerName = "namenode"
-    val command = Seq("sh", "-c", s"""docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $containerName""")
-    val result = command.!!
-    val numeros: Array[Int] = result.trim.split('.').map(_.toInt)
-    val direccionIP: String = numeros.mkString(".")
-    val hdfsUri: String = s"hdfs://$direccionIP:9000"
-    val hdfsWorkingDir = "/user/fede/landing/csv"
-    hdfs.runloadinHdfs(hadoopConf, archivos, hdfsUri, hdfsWorkingDir)
     val spark = SparkSessionSingleton.getSparkSession
     funcTransform.processFile(spark, archivos)
     spark.stop()
